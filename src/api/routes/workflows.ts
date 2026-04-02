84df9fc192513a83234c0f71b0aae568957fd0d7
Joab Rodrigues <joabrodriguesfilho@gmail.com>
2026-04-01 20:46:15 -0400

    removing older project

--- src/api/routes/workflows.ts
+++
@@ -1,6 +0,0 @@
-import type { FastifyInstance } from "fastify";
-import { listWorkflows } from "../../../workflows/loader.js";
-
-export async function workflowsRoutes(app: FastifyInstance) {
-  app.get("/workflows", async () => ({ workflows: listWorkflows() }));
-}
